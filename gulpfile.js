feb50a73cedbff56a75f257885161326ec9ba60b
Craig Humphreys <craig_h_411@yahoo.co.uk>
2017-01-08 19:12:28 +0000

    update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
+var umd = require('gulp-umd');
 
 gulp.task('default', function(cb) {
      pump([
@@ -10,4 +11,10 @@ gulp.task('default', function(cb) {
     ],
     cb
   );
+});
+
+gulp.task('umd', function() {
+  return gulp.src('src/*.js')
+    .pipe(umd())
+    .pipe(gulp.dest('umd'));
 });
